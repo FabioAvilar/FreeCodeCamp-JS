b5f30dace27d3c6c65f786111fcc66c68eb185ea
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-21 16:13:12 +0200

    assigning the value of one variable to another

---
+++ basic-javascript/assigning-the-value-of-one-variable-to-another.js
@@ -0,0 +1,12 @@
+// You should not change code above the specified comment.
+// Passed:b should have a value of 7.
+// Passed:a should be assigned to b with =.
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+var b = a;
