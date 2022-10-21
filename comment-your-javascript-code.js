10d22cb3d62cc2aa93b63e85b3311a62fb15046d
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-21 16:00:13 +0200

    ex1 comment your js code

---
+++ comment-your-javascript-code.js
@@ -0,0 +1,8 @@
+// You should create a // style comment that contains at least five letters.
+// Waiting:You should create a /* */ style comment that contains at least five letters.
+
+// This is an in-line comment
+
+
+/* This is a
+multi-line comment */
