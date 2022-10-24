8b0892c8c1116e15dc196bbf38785fe925e3837c
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-24 11:19:27 +0200

    exercises

---
+++ basic-javascript/declare-string-variables.js
@@ -0,0 +1,7 @@
+// Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+// myFirstName should be a string with at least one character in it.
+// Waiting:myLastName should be a string with at least one character in it.
+
+var myFirstName = "Fabio";
+var myLastName = "Bassani";
---
+++ basic-javascript/understanding-uninitialized-variables.js
@@ -0,0 +1,16 @@
+// Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+
+// a should be defined and have a final value of 6.
+// Waiting:b should be defined and have a final value of 15.
+// Waiting:c should not contain undefined and should have a final value of the string I am a String!
+// Waiting:You should not change code below the specified comment.
+
+// Only change code below this line
+var a = 6;
+var b;
+var c;
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
