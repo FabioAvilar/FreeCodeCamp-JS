89a80bba1a7bbe3af2ead8b768724561c28c5137
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-26 08:54:26 +0200

    exercises freeCodeCamp

---
+++ basic-javascript/explore-differences-between-the-var-and-let-keywords.js
@@ -0,0 +1,11 @@
+// Update the code so it only uses the let keyword.
+
+// var should not exist in the code.
+// Waiting: catName should be the string Oliver.
+// Waiting: catSound should be the string Meow!
+
+// var catName = "Oliver";
+// var catSound = "Meow!";
+
+let catName = "Oliver";
+let catSound = "Meow!";
