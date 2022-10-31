c543095eef4ede79cb2426d1a132fee7650002d5
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-31 09:06:19 +0100

    exercises JS

---
+++ basic-javascript/escaping-literal-quotes-in-strings.js
@@ -0,0 +1,12 @@
+// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+// I am a "double quoted" string inside "double quotes".
+
+// You should use two double quotes (") and four escaped double quotes (\").
+// Waiting:Variable myStr should contain the string: I am a "double quoted" string inside "double quotes".
+
+// const myStr = ""; // Change this line
+
+const myStr = 'I am a "double quoted" string inside "double quotes".'; // Change this line
+
+console.log(myStr);
