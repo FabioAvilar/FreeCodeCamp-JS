eb38c3b5997deae35b1215f6d3ea40c36915406e
FabioBassani <avilar.fabio@yahoo.com.br>
2022-11-07 20:48:53 +0100

    ex shopping list

---
+++ basic-javascript/shopping-list.js
@@ -0,0 +1,16 @@
+// There should be at least 5 sub-arrays in the list.
+
+// Passed:myList should be an array.
+// Passed:The first elements in each of your sub-arrays should all be strings.
+// Passed:The second elements in each of your sub-arrays should all be numbers.
+// Passed:You should have at least 5 items in your list.
+
+// const myList = [];
+
+const myList = [
+    ["fabio", 30],
+    ["debora", 29],
+    ["hygor", 19],
+    ["debora", 29],
+    ["hygor", 19],
+];
