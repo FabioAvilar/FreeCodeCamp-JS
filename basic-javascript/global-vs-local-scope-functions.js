b27d6a187847567f071b285fb3f7e0b6aeee880e
FabioBassani <avilar.fabio@yahoo.com.br>
2022-11-15 09:47:48 +0100

    ex JS

---
+++ basic-javascript/global-vs-local-scope-functions.js
@@ -0,0 +1,29 @@
+// Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+// You should not change the value of the global outerWear.
+// Waiting:myOutfit should return the string sweater.
+// Waiting:You should not change the return statement.
+
+// // Setup
+// const outerWear = "T-Shirt";
+
+// function myOutfit() {
+//   // Only change code below this line
+  
+//   // Only change code above this line
+//   return outerWear;
+// }
+
+// myOutfit();
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
