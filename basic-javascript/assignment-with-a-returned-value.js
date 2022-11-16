24d8a6b99882696197f5a95f99d3e15b88cc0b6c
FabioBassani <avilar.fabio@yahoo.com.br>
2022-11-16 08:00:16 +0100

    ex JS

---
+++ basic-javascript/assignment-with-a-returned-value.js
@@ -0,0 +1,23 @@
+// Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+// Waiting:processed should have a value of 2
+// Waiting:You should assign processArg to processed
+
+// // Setup
+// let processed = 0;
+
+// function processArg(num) {
+//   return (num + 3) / 5;
+// }
+
+// // Only change code below this line
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
---
+++ basic-javascript/understanding-undefined-value-returned.js
@@ -0,0 +1,40 @@
+// Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+
+// Waiting:addFive should be a function.
+// Waiting:Once both functions have run, the sum should be equal to 8.
+// Waiting:Returned value from addFive should be undefined.
+// Waiting:Inside the addFive function, you should add 5 to the sum variable.
+
+// // Setup
+// let sum = 0;
+
+// function addThree() {
+//   sum = sum + 3;
+// }
+
+// // Only change code below this line
+// function addFive() {
+//   sum += 5;
+// }
+
+// // Only change code above this line
+
+// addThree();
+// addFive();
+
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
